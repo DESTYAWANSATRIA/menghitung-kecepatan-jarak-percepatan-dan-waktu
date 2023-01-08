0012759edddd3f1c510bd70f6d58fcc50599233c
DESTYAWANSATRIA <121388974+DESTYAWANSATRIA@users.noreply.github.com>
2023-01-08 20:09:44 +0700

    Update pengukur jarak.asm

--- pengukur jarak.asm
+++ pengukur jarak.asm
@@ -1,68 +1,245 @@
+org 100h
 
-NAME "DESTYAWAN"
-INCLUDE 'emu8086.inc'
-ORG 100H
-JMP MULAI
-PSN1    DB "||   MENGHITUNG JARAK  ||",13,10
-        DB "=========================",13,10,13,10,'$'
-V0      DB "MASUKKAN KECEPATAN      : $"
-t       DB "MASUKAN WAKTU           : $"
-S       DB "JARAK                   : $"
-PJG     DW ?
-TOT     DW ?
-ARE     DW ?
+jmp start      
+
+hd:     db       "===============================================================",0dh,0ah,'$'
+hd1     db       "|| PROGRAM MENGHITUNG JARAK, KECEPATAN, WAKTU DAN PERCEPATAN ||",0dh,0ah,'$'
+hd2     db       "===============================================================", 0Dh,0Ah,'$'
+msg:    db      "1. kecepatan",0dh,0ah,"2. Jarak ",0dh,0ah,"3. percepatan",0dh,0ah,"4. Percepatan",0dh,0ah,"   masukan pilihan", 0Dh,0Ah, '$'
+msg2:    db      0dh,0ah,"masukan kecepatan (m/s)      : $"
+msg3:    db      0dh,0ah,"masukan percepatan (m/s^2)   : $"
+msg4:    db      0dh,0ah,"masukan waktu tempuh (s)     : $"
+msg5:    db      0dh,0ah,"masukan jarak (m)            : $"
+msg6:    db      0dh,0ah," Error $" 
+msg7:    db      0dh,0ah,"Hasil : $" 
+msg8:    db      0dh,0ah ,'Terimakasih !!!!  press any key... ', 0Dh,0Ah, '$'
+                
+                
+start:  mov ah,9
+        mov dx, offset hd
+        int 21h
+        mov ah,9
+        mov dx, offset hd1
+        int 21h
+        mov ah,9
+        mov dx, offset hd2
+        int 21h
+        mov ah,9
+        mov dx, offset msg
+        int 21h
+        mov ah,0                       
+        int 16h  
+        cmp al,31h 
+        je kecptn
+        cmp al,32h
+        je Jarak
+        cmp al,33h
+        je waktu
+        cmp al,34h
+        je prcptn
+        mov ah,09h
+        mov dx, offset msg4
+        int 21h
+        mov ah,0
+        int 16h
+        jmp start
+        
+kecptn:     mov ah,09h
+            mov dx, offset msg5
+            int 21h
+            mov cx,0
+            call InputNo
+            push dx
+            mov ah,9
+            mov dx, offset msg4
+            int 21h 
+            mov cx,0
+            call InputNo
+            pop bx
+            mov ax,bx
+            mov cx,dx
+            mov dx,0
+            mov bx,0
+            div cx
+            mov bx,dx
+            mov dx,ax
+            push bx 
+            push dx 
+            mov ah,9
+            mov dx, offset msg7
+            int 21h
+            mov cx,10000
+            pop dx
+            call View
+            pop bx
+            cmp bx,0
+            je exit 
+            jmp exit             
+
+            
+InputNo:    mov ah,0
+            int 16h 
+            mov dx,0  
+            mov bx,1 
+            cmp al,0dh
+            je FormNo 
+            sub ax,30h 
+            call ViewNo 
+            mov ah,0
+            push ax 
+            inc cx   
+            jmp InputNo          
+   
 
 
-MULAI:
-LEA DX,PSN1
-MOV AH,9
-INT 21H
+FormNo:     pop ax  
+            push dx      
+            mul bx
+            pop dx
+            add dx,ax
+            mov ax,bx       
+            mov bx,10
+            push dx
+            mul bx
+            pop dx
+            mov bx,ax
+            dec cx
+            cmp cx,0
+            jne FormNo
+            ret   
 
-LEA DX,V0
-MOV AH,9
-INT 21H
 
-CALL SCAN_NUM
+       
+       
+View:  mov ax,dx
+       mov dx,0
+       div cx 
+       call ViewNo
+       mov bx,dx 
+       mov dx,0
+       mov ax,cx 
+       mov cx,10
+       div cx
+       mov dx,bx 
+       mov cx,ax
+       cmp ax,0
+       jne View
+       ret
 
-MOV PJG,CX;KECEPATAN
 
-PUTC 13
-PUTC 10
+ViewNo:    push ax
+           push dx
+           mov dx,ax 
+           add dl,30h 
+           mov ah,2
+           int 21h
+           pop dx  
+           pop ax
+           ret
+      
+   
+exit:   mov dx,offset msg8
+        mov ah, 09h
+        int 21h  
 
-LEA DX,t
-MOV AH,9
-INT 21H
 
-CALL SCAN_NUM  ;WAKTUUU
+        mov ah, 0
+        int 16h
 
-MOV TOT,CX 
+        ret
+            
+                       
+Jarak:   mov ah,09h
+            mov dx, offset msg2
+            int 21h
+            mov cx,0
+            call InputNo
+            push dx
+            mov ah,9
+            mov dx, offset msg4
+            int 21h 
+            mov cx,0
+            call InputNo
+            pop bx
+            mov ax,dx
+            mul bx 
+            mov dx,ax
+            push dx
+ 
+            mov ah,9
+            mov dx, offset msg7
+            int 21h
+            mov cx,10000
+            pop dx
+            call View 
+            jmp exit 
 
-MOV AX,TOT
-MOV BX,PJG ;V0 . T
-MUL BX
-PUSH AX
 
-PUTC 13
-PUTC 10
+waktu:     mov ah,09h
+            mov dx, offset msg5
+            int 21h
+            mov cx,0
+            call InputNo
+            push dx
+            mov ah,9
+            mov dx, offset msg2
+            int 21h 
+            mov cx,0
+            call InputNo
+            pop bx
+            mov ax,bx
+            mov cx,dx
+            mov dx,0
+            mov bx,0
+            div cx
+            mov bx,dx
+            mov dx,ax
+            push bx 
+            push dx 
+            mov ah,9
+            mov dx, offset msg7
+            int 21h
+            mov cx,10000
+            pop dx
+            call View
+            pop bx
+            cmp bx,0
+            je exit 
+            jmp exit             
 
-LEA DX,S
-MOV AH,9
-INT 21H
+                
+    
+            
+prcptn:     mov ah,09h
+            mov dx, offset msg5
+            int 21h
+            mov cx,0
+            call InputNo
+            push dx
+            mov ah,9
+            mov dx, offset msg2
+            int 21h 
+            mov cx,0
+            call InputNo
+            pop bx
+            mov ax,bx
+            mov cx,dx
+            mov dx,0
+            mov bx,0
+            div cx
+            mov bx,dx
+            mov dx,ax
+            push bx 
+            push dx 
+            mov ah,9
+            mov dx, offset msg7
+            int 21h
+            mov cx,10000
+            pop dx
+            call View
+            pop bx
+            cmp bx,0
+            je exit 
+            jmp exit             
 
-POP AX
-MOV ARE,AX
-
-CALL PRINT_NUM
-
-PUTC 13
-PUTC 10
-
-MOV AH,0
-INT 16H
-
-RET
-
-DEFINE_SCAN_NUM
-DEFINE_PRINT_NUM
-DEFINE_PRINT_NUM_UNS
-END
+                
